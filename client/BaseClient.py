6d4b8b802ea1015c36ad0bdb1d341cb8af2c5c4f
Kimberly <kimberlyo3113@gmail.com>
2025-11-14 11:02:47 -0500

    3 create base client class

---
+++ client/BaseClient.py
@@ -0,0 +1,18 @@
+import httpx
+class BaseClient:
+  def __init__(self, token: str):
+    self.baseUrl = 'https://api.spacetraders.io/v2'
+    self.session = httpx.Client(
+      headers={"Authorization": f"Bearer {token}","Content-Type": "application/json"}
+    )
+    
+  def _handle(self,response):
+    return response.json()["data"]
+  
+  def get(self, path):
+        res = self.session.get(self.baseUrl + path)
+        return self._handle(res)
+  
+  def post(self, path, payload):
+    res = self.session.post(self.baseUrl + path, json=payload or {})
+    return self._handle(res)
