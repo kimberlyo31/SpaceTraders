d534ea5f8ff52916d8bf101d91db05c00a3cd395
Kimberly <kimberlyo3113@gmail.com>
2025-11-14 12:05:02 -0500

    create agent api client

---
+++ client/AgentClient.py
@@ -0,0 +1,8 @@
+from BaseClient import BaseClient
+
+class AgentClient(BaseClient):
+  def get_my_agent(self):
+    return self.get("/my/agent")
+  
+  def get_agent_events(self):
+    return self.get("/my/agent/events")
