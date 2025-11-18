d710ba19975c36943150e91edbb622cb8312725d
Kimberly <kimberlyo3113@gmail.com>
2025-11-17 21:04:07 -0500

    added more items to system module

--- models/chart.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Chart:
-  
-  def from_json(payload):
-    return Chart()
-  
--- models/system.py
+++ models/system.py
@@ -1,13 +1,17 @@
 from dataclasses import dataclass
 from enums import SystemType, WaypointType, WaypointModifierSymbol, WaypointTraitSymbol
 from faction import Faction
+from agent import Agent
 
 @dataclass
 class WaypointOrbital:
   symbol: str
   
-  def from_json(payload):
-    return WaypointOrbital()
+  @staticmethod
+  def from_json(payload: dict):
+    return WaypointOrbital(
+      symbol=payload['symbol']
+    )
 
 @dataclass
 class WaypointModifier:
@@ -15,7 +19,8 @@ class WaypointModifier:
   name: str
   description: str
   
-  def from_json(payload):
+  @staticmethod
+  def from_json(payload: dict):
     return WaypointTrait(
       symbol=WaypointModifierSymbol(payload['symbol']),
       name=payload['name'],
@@ -28,7 +33,8 @@ class WaypointTrait:
   name: str
   description: str
   
-  def from_json(payload):
+  @staticmethod
+  def from_json(payload: dict):
     return WaypointTrait(
       symbol=WaypointTraitSymbol(payload['symbol']),
       name=payload['name'],
@@ -36,19 +42,52 @@ class WaypointTrait:
     )
   
 @dataclass
+class Chart:
+  waypoint_symbol: str
+  submitted_by: Agent
+  submitted_on: str
+  
+  @staticmethod
+  def from_json(payload: dict):
+    return Chart(
+      waypoint_symbol=payload['waypointSymbol'],
+      submitted_by=Agent(payload['submittedBy']),
+      submitted_on=payload['submittedOn']
+    )
+@dataclass
 class Waypoint:
   symbol: str
-  type: WaypointType
+  waypoint_type: WaypointType
+  system_symbol: str
   x: int
   y: int
   orbitals: list[WaypointOrbital]
-  orbits: str
-  def from_json(payload):
-    return Waypoint()
+  traits: list[WaypointTrait]
+  isUnderConstruction: bool
+  orbits: str | None = None
+  faction: Faction | None = None
+  modifiers: list[WaypointModifier] | None = None
+  chart: Chart | None = None
+  
+  @staticmethod
+  def from_json(payload: dict):
+    return Waypoint(
+      symbol=payload['symbol'],
+      type=WaypointType(payload['type']),
+      system_symbol=payload['systemSymbol'],
+      x=payload['x'],
+      y=payload['y'],
+      orbitals=[WaypointOrbital.from_json(o) for o in payload['orbitals']],
+      traits=[WaypointTrait.from_json(t) for t in payload['traits']],
+      isUnderConstruction=payload['isUnderConstruction'],
+      orbits=payload['orbits'],
+      faction=Faction.from_json(payload['faction']),
+      modifiers=[WaypointTrait.from_json(m) for m in payload['modifiers']],
+      chart=Chart.from_json(payload['chart'])
+    )
   
 @dataclass
 class System:
-  contsellation: str
   symbol: str
   sector_symbol: str
   type: SystemType
@@ -56,7 +95,19 @@ class System:
   y: int
   waypoints: list[Waypoint]
   factions: list[Faction]
-  name: str
+  contsellation: str | None = None
+  name: str | None = None
   
-  def from_json(payload):
-    return System()
+  @staticmethod
+  def from_json(payload: dict) -> "System":
+    return System(
+      symbol=payload["symbol"],
+      sector_symbol=payload["sectorSymbol"],
+      type=SystemType(payload["type"]),
+      x=payload["x"],
+      y=payload["y"],
+      waypoints=[Waypoint.from_json(wp) for wp in payload["waypoints"]],
+      factions=[Faction.from_json(f) for f in payload.get("factions", [])],
+      constellation=payload["constellation"],
+      name=payload["name"]
+    )
